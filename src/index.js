4372202408dd9064dbbd8f3bedd6be4166020a83
Jesse <jessejkilback@gmail.com>
2022-09-28 18:49:31 -0700

    add buttons and style loader

--- src/index.js
+++ src/index.js
@@ -6,3 +6,18 @@ header.classList.add('header');
 header.innerText = 'The Restaraunt'
 
 content.appendChild(header);
+
+let tabContainer = document.createElement('div');
+let homeBtn = document.createElement('button');
+let aboutBtn = document.createElement('button');
+let contactBtn = document.createElement('button');
+
+homeBtn.textContent = "Home";
+aboutBtn.textContent = "About";
+contactBtn.textContent = "Contact";
+
+tabContainer.appendChild(homeBtn);
+tabContainer.appendChild(aboutBtn);
+tabContainer.appendChild(contactBtn);
+
+content.appendChild(tabContainer);
